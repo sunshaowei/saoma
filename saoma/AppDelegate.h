ef26b475cf33b4cb068fea88f46c4b8d7da3333e
sunshaowei <sunshaowei@kuanter.com>
2016-11-23 18:39:01 +0800

    系统扫码
    只是支系统扫码进行封装，无需引用第三方依赖库

---
+++ saoma/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  saoma
+//
+//  Created by kuanter on 16/6/7.
+//  Copyright © 2016年 kuanter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ saoma/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  saoma
+//
+//  Created by kuanter on 16/6/7.
+//  Copyright © 2016年 kuanter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
